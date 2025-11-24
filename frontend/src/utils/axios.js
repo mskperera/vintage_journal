89794b020ba3381eade86aa7c7cbbaa90f9ed8ee
Susantha Perera <33259705+mskperera@users.noreply.github.com>
2025-11-24 21:18:03 +0530

    Update axios.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const customAxios = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE || 'http://localhost:5000',
+  baseURL: import.meta.env.VITE_API_BASE || 'https://vintagejournal.legendbyte.com',
 });
 
 customAxios.interceptors.request.use(
